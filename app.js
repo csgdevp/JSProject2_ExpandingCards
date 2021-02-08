cdc3bb2866fee50831ce2f8ca9cdccb7e37cd3d6
Chandra Gonuguntla <csgdevp@gmail.com>
2021-02-08 16:12:53 -0500

    Expanded cards projects
    JavaScript Based project

---
+++ app.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll(".panel");
+for (let panel of panels) {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+}
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
